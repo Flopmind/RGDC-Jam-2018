40952f8a84d66761c035b90517e1ac2b0b3b0437
speebus46 <44912423+speebus46@users.noreply.github.com>
2018-11-10 11:15:45 -0500

    Camera movement script

--- UPS - United Potion Service/Assets/Scripts/CameraScript.cs
+++ UPS - United Potion Service/Assets/Scripts/CameraScript.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
-
+    public GameObject player;
+    public float threshold;
+    public float cameraTime;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Vector3 playerPos = player.transform.position;
+        if (Mathf.Abs(transform.position.y-player.transform.position.y)>threshold)
+        {
+            Vector3.Lerp(transform.position, player.transform.position, cameraTime);
+        }
 	}
 }
