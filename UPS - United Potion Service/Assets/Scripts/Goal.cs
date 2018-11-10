0df40eb589c6731800237b29485d2985620afc28
Caleb Katzenstein (RIT Student) <cnk5777@ad.rit.edu>
2018-11-10 16:32:43 -0500

    Added more GUI

---
+++ UPS - United Potion Service/Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			GameObject.Find("ScoreText").GetComponent<Text>().enabled = true;
+		}
+	}
+}
