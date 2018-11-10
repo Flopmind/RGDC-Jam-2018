a3507107d54e609fb9afc39dc519216482c7f4e4
Caleb Katzenstein (RIT Student) <cnk5777@ad.rit.edu>
2018-11-09 22:26:26 -0500

    Potions explode where the mouse is

--- UPS - United Potion Service/Assets/Scripts/Potion.cs
+++ UPS - United Potion Service/Assets/Scripts/Potion.cs
@@ -6,17 +6,32 @@ public class Potion : MonoBehaviour
 {
 	[SerializeField]
 	float timeUntilEffect = 1;
+	Vector3 activationLocation;
+	public Vector3 ActivationLocation
+	{
+		get
+		{
+			return activationLocation;
+		}
+		set
+		{
+			activationLocation = value;
+		}
+	}
 	// Use this for initialization
 	void Start () 
 	{
-		StartCoroutine(WaitUntilEffect());
+		//StartCoroutine(WaitUntilEffect());
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if (Vector3.SqrMagnitude(transform.position - activationLocation) < .01)
+		{
+			TriggerEffect();
+		}
 	}
 
 	IEnumerator WaitUntilEffect()
