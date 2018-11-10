da24963fc55295691416355e46653a7dc4f0de34
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-11-09 21:21:53 -0500

    health script created

---
+++ UPS - United Potion Service/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+    [SerializeField]
+    private int maxHealth;
+    private int currentHealth;
+
+    public int Health
+    {
+        get { return currentHealth; }
+        set
+        {
+            if (value < 0)
+            {
+                currentHealth = 0;
+            }
+            else if (value > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+            else
+            {
+                currentHealth = value;
+            }
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health = Health - damage;
+    }
+}
