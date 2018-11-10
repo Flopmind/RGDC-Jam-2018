15539c9578b4d5bb5e109fb09bd021060ee53b36
Tom Slavin (RIT Student) <tps3871@ad.rit.edu>
2018-11-10 12:13:32 -0500

    Player animation

--- UPS - United Potion Service/Assets/Scripts/Player Scripts/PlayerMovementScript.cs
+++ UPS - United Potion Service/Assets/Scripts/Player Scripts/PlayerMovementScript.cs
@@ -13,7 +13,16 @@ public class PlayerMovementScript : VehicleScript {
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
+            GetComponent<Animator>().SetBool("Running", true); // update Running value to reflect input
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                GetComponent<Animator>().SetBool("FacingR", Input.GetAxis("Horizontal") > 0); // update FacingR
+            }
             return (new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Running", false); // update Running value to reflect input
         }
         return Vector3.zero;
     }
