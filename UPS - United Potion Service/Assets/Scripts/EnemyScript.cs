53b946b11f59dd3053750af646d9b9c786cfcf94
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-11-09 21:45:49 -0500

    Made Vehicle Script require rigidbody 2d, made absract enemy script that requires a health script

---
+++ UPS - United Potion Service/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthScript))]
+public abstract class EnemyScript : VehicleScript {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
--- UPS - United Potion Service/Assets/Scripts/VehicleScript.cs
+++ UPS - United Potion Service/Assets/Scripts/VehicleScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public abstract class VehicleScript : MonoBehaviour {
 
     [SerializeField]
