c0dc433445e2ffd23187345f4810a8a47c5c3793
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-11-09 21:58:52 -0500

    improved EnemyScript

--- UPS - United Potion Service/Assets/Scripts/EnemyScript.cs
+++ UPS - United Potion Service/Assets/Scripts/EnemyScript.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(HealthScript))]
 public abstract class EnemyScript : VehicleScript {
 
+    [SerializeField]
+    protected float aggroRange;
+
+    protected GameObject player;
+    protected GameObject target = null;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+            throw new System.ArgumentNullException("Player not found in EnemyScript");
 	}
 	
 	// Update is called once per frame
@@ -16,4 +24,12 @@ public abstract class EnemyScript : VehicleScript {
     {
 		
 	}
+
+    protected void TargetPlayer()
+    {
+        if (player && (transform.position - player.transform.position).magnitude <= aggroRange)
+        {
+            target = player;
+        }
+    }
 }
