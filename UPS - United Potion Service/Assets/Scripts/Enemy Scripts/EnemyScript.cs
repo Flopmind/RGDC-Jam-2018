eed1595759fa4584ecdd4a710d7bdd826306242b
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-11-11 13:16:37 -0500

    fixed double damage glitch

--- UPS - United Potion Service/Assets/Scripts/Enemy Scripts/EnemyScript.cs
+++ UPS - United Potion Service/Assets/Scripts/Enemy Scripts/EnemyScript.cs
@@ -52,18 +52,6 @@ public abstract class EnemyScript : VehicleScript {
         }
     }
 
-    protected virtual void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            HealthScript otherHealth = other.gameObject.GetComponent<HealthScript>();
-            if (otherHealth != null)
-            {
-                otherHealth.TakeDamage(attackDamage);
-            }
-        }
-    }
-
     // Figures out variables to plug into the animator system
     protected void Animate(Vector3 motion)
     {
