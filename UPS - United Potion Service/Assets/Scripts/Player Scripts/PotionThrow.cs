4ca5fabed5fd07974fc99530ced9a34dac611f39
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-11-10 20:26:52 -0500

    fixed potion throw code

--- UPS - United Potion Service/Assets/Scripts/Player Scripts/PotionThrow.cs
+++ UPS - United Potion Service/Assets/Scripts/Player Scripts/PotionThrow.cs
@@ -13,17 +13,19 @@ public class PotionThrow : MonoBehaviour
     [SerializeField]
     private List<GameObject> myPotions;
     [SerializeField]
-    private List<int> potionsCounts;
+    private List<uint> potionsCounts;
 
     private GameObject[] enemies;
     private int index;
     private float throwTimer = 0;
-    PlayerInventory inv;
     private void Start()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         index = 0;
-        inv = GetComponent<PlayerInventory>();
+        if (myPotions.Count != potionsCounts.Count)
+        {
+            throw new System.ArgumentNullException("Bad matchup of potions and potion counts");
+        }
     }
 
     // checks for input, instantiates potion and makes potion move
@@ -31,32 +33,28 @@ public class PotionThrow : MonoBehaviour
     {
         if (throwTimer <= 0)
         {
-            GameObject potionToInstantiate = null;
-            if (Input.GetMouseButtonDown(0) && myPotions.Count > 0)
+            if (Input.GetMouseButtonDown(0) && myPotions.Count > 0 && potionsCounts[index] != 0)
             {
-                potionToInstantiate = inv.RetrieveItem((PlayerInventory.PotionType)index);
-                if (potionToInstantiate != null)
+                Vector3 vecToMouse = (MousePos.MousePosition - transform.position);
+                float distanceTravel = vecToMouse.magnitude;
+                vecToMouse.Normalize();
+                GameObject potionInstance = Instantiate(myPotions[index], transform.position + vecToMouse, Quaternion.identity);
+                if (potionInstance.GetComponent<ThrownPotion>())
                 {
-                    Vector3 vecToMouse = (MousePos.MousePosition - transform.position);
-                    float distanceTravel = vecToMouse.magnitude;
-                    vecToMouse.Normalize();
-                    GameObject potionInstance = Instantiate(potionToInstantiate, transform.position + vecToMouse, Quaternion.identity);
-                    if (potionInstance.GetComponent<ThrownPotion>())
-                    {
-                        potionInstance.GetComponent<ThrownPotion>().ActivationLocation = MousePos.MousePosition;
-                        potionInstance.GetComponent<ThrownPotion>().SetDistances(transform.position, maxThrowRange, distanceTravel);
-                        potionInstance.transform.up = vecToMouse;
-                        potionInstance.GetComponent<Rigidbody2D>().velocity = vecToMouse * potionThrowSpeed;
-                        throwTimer = throwInterval;
-                    }
-                    else if (potionInstance.GetComponent<DrinkablePotion>())
-                    {
-                        potionInstance.GetComponent<DrinkablePotion>().Initialize();
-                        potionInstance.GetComponent<DrinkablePotion>().Drink();
-                        Destroy(potionInstance);
-                    }
+                    potionInstance.GetComponent<ThrownPotion>().ActivationLocation = MousePos.MousePosition;
+                    potionInstance.GetComponent<ThrownPotion>().SetDistances(transform.position, maxThrowRange, distanceTravel);
+                    potionInstance.transform.up = vecToMouse;
+                    potionInstance.GetComponent<Rigidbody2D>().velocity = vecToMouse * potionThrowSpeed;
                     throwTimer = throwInterval;
                 }
+                else if (potionInstance.GetComponent<DrinkablePotion>())
+                {
+                    potionInstance.GetComponent<DrinkablePotion>().Initialize();
+                    potionInstance.GetComponent<DrinkablePotion>().Drink();
+                    Destroy(potionInstance);
+                }
+                throwTimer = throwInterval;
+                --potionsCounts[index];
             }
         }
         else
